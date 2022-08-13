3983a34b8c7bd3e78eb223649acd0f5b0184d601
Marcus <hamelink.m@gmail.com>
2022-08-13 15:07:04 +0200

    redesign first steps

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+// .d1 {
+//   width: 100%;
+//   height: 50px;
+//   background-color: #354b8c;
+// }
+// .d2 {
+//   width: 100%;
+//   height: 50px;
+//   background-color: #4478a6;
+// }
+// .d3 {
+//   width: 100%;
+//   height: 50px;
+//   background-color: #faf3dd;
+// }
+// .d4 {
+//   width: 100%;
+//   height: 50px;
+//   background-color: #f27b35;
+// }
+// .d5 {
+//   width: 100%;
+//   height: 50px;
+//   background-color: #00bfb3;
+// }
